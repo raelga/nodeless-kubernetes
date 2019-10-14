bfe282c9d90f1f16811cedb0ab88b15a7a5de917
Roi Vazquez <roivazquez@gmail.com>
2019-10-14 22:51:48 +0200

    Clean up eks terrraform file

--- labs/fargate-eks/terraform/eks.tf
+++ labs/fargate-eks/terraform/eks.tf
@@ -2,19 +2,11 @@ locals {
   eks_cluster_name = "lab-eks"
 }
 
-resource "aws_cloudwatch_log_group" "lab-lg" {
-  name              = "/aws/eks/${local.eks_cluster_name}/cluster"
-  retention_in_days = "1"
-  # kms_key_id        = var.cluster_log_kms_key_id
-  # tags              = var.tags
-}
-
 resource "aws_eks_cluster" "eks" {
-  name     = local.eks_cluster_name
-  role_arn = aws_iam_role.cluster.arn
-  version  = "1.14"
-  # enabled_cluster_log_types = var.cluster_enabled_log_types
-  # tags                      = var.tags
+  name                      = local.eks_cluster_name
+  role_arn                  = aws_iam_role.cluster.arn
+  version                   = "1.14"
+  enabled_cluster_log_types = ["api", "scheduler", "controllerManager", "authenticator", "api"]
 
   vpc_config {
     security_group_ids = [aws_security_group.cluster.id]
@@ -29,20 +21,22 @@ resource "aws_eks_cluster" "eks" {
   depends_on = [
     aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy,
     aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy,
-    # aws_cloudwatch_log_group.this
+    aws_cloudwatch_log_group.cluster
   ]
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "eks_cluster_ca" {
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
 resource "aws_security_group" "cluster" {
   name_prefix = "eks"
   description = "EKS cluster security group."
   vpc_id      = module.vpc.vpc_id
-  # tags = merge(
-  #   var.tags,
-  #   {
-  #     "Name" = "${var.eks_cluster_name}-eks_cluster_sg"
-  #   },
-  # )
 }
 
 resource "aws_security_group_rule" "cluster_egress_internet" {
@@ -54,16 +48,6 @@ resource "aws_security_group_rule" "cluster_egress_internet" {
   to_port           = 0
   type              = "egress"
 }
-
-# resource "aws_security_group_rule" "cluster_https_worker_ingress" {
-#   description              = "Allow pods to communicate with the EKS cluster API."
-#   protocol                 = "tcp"
-#   security_group_id        = aws_security_group.cluster.id
-#   source_security_group_id = local.worker_security_group_id
-#   from_port                = 443
-#   to_port                  = 443
-#   type                     = "ingress"
-# }
 
 resource "aws_iam_role" "cluster" {
   name_prefix        = "eks"
@@ -90,4 +74,9 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.cluster.name
+}
+
+resource "aws_cloudwatch_log_group" "cluster" {
+  name              = "/aws/eks/${local.eks_cluster_name}/cluster"
+  retention_in_days = "1"
 }
