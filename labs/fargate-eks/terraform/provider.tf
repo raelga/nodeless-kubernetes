269e940eda8767eff7882c2623024b5a7c045e53
Roi Vazquez <roivazquez@gmail.com>
2019-10-14 22:52:27 +0200

    Fix indentation

--- labs/fargate-eks/terraform/provider.tf
+++ labs/fargate-eks/terraform/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket     = "tf-roi"
-    key        = "terraform.tfstate"
-    region     = "eu-west-1"
+    bucket = "tf-roi"
+    key    = "terraform.tfstate"
+    region = "eu-west-1"
 
   }
 }
 
 provider "aws" {
-  version    = ">= 2.28.1"
-  region     = "eu-west-1"
+  version = ">= 2.28.1"
+  region  = "eu-west-1"
 }
